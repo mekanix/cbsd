89fdb923a865eed5a1060c24b8ed7e9d7df08c7d
Goran Mekić <meka@tilda.center>
2018-06-16 20:50:48 +0200

    Add getopt parser

--- sock/shell/cbsdsh.cpp
+++ sock/shell/cbsdsh.cpp
@@ -2,15 +2,18 @@
 #include <thread>
 #include <chrono>
 #include "shell.h"
+#include "CLI/CLI.hpp"
 
 
-int main()
+int main(int argc, char **argv)
 {
-  Shell s("/tmp/cbsd.sock");
-  while(1)
-  {
-    s.put("quit");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
+  CLI::App app{"CBSD shell"};
+  // Shell s("/tmp/cbsd.sock");
+  auto construct = app.add_subcommand("construct", "Construct resource");
+  auto start = app.add_subcommand("start", "Start resource");
+  auto stop = app.add_subcommand("stop", "Stop resource");
+  app.require_subcommand(1);
+  CLI11_PARSE(app, argc, argv);
+  std::cout << "Subcommand: " << app.get_subcommands()[0]->get_name() << std::endl;
   return 0;
 }
