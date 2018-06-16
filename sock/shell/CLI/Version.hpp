4fe100fb8b472339a52e4066d7e9fbdda029a15c
Goran Mekić <meka@tilda.center>
2018-06-16 20:50:48 +0200

    Add getopt parser

---
+++ sock/shell/CLI/Version.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+// Distributed under the 3-Clause BSD License.  See accompanying
+// file LICENSE or https://github.com/CLIUtils/CLI11 for details.
+
+// [CLI11:verbatim]
+
+#define CLI11_VERSION_MAJOR 1
+#define CLI11_VERSION_MINOR 5
+#define CLI11_VERSION_PATCH 4
+#define CLI11_VERSION "1.5.4"
+
+// [CLI11:verbatim]
--- sock/shell/cbsdsh.cpp
+++ sock/shell/cbsdsh.cpp
@@ -2,15 +2,32 @@
 #include <thread>
 #include <chrono>
 #include "shell.h"
+#include "CLI/CLI.hpp"
 
 
-int main()
+std::string jailName;
+
+
+int main(int argc, char **argv)
 {
-  Shell s("/tmp/cbsd.sock");
-  while(1)
+  CLI::App app{"CBSD shell"};
+  app.require_subcommand(1);
+  // Shell s("/tmp/cbsd.sock");
+
+  // Start command
   {
-    s.put("quit");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    auto subcommand = app.add_subcommand("start", "Start resource");
+    subcommand->add_option("jail", jailName, "Jail name")->required();
   }
+
+  // Stop command
+  {
+    auto subcommand = app.add_subcommand("stop", "Stop resource");
+    subcommand->add_option("jail", jailName, "Jail name")->required();
+  }
+
+  CLI11_PARSE(app, argc, argv);
+  std::cout << "Subcommand: " << app.get_subcommands()[0]->get_name() << std::endl;
+  std::cout << "Jail name: " << jailName << std::endl;
   return 0;
 }
