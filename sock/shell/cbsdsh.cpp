6ef069dd8d37282c1e5e888c849bfcc6b4143724
Goran Mekić <meka@tilda.center>
2018-10-06 05:43:57 +0200

    Allow multiple jail arguments

--- sock/shell/cbsdsh.cpp
+++ sock/shell/cbsdsh.cpp
@@ -3,23 +3,37 @@
 
 #include <chrono>
 #include <iostream>
+#include <map>
 #include <thread>
 #include <vector>
 
-std::string jail;
-std::string result;
+std::map<std::string, std::string> options;
+std::vector<std::string> jails;
 
 int main(int argc, char **argv)
 {
   CLI::App app{"CBSD shell"};
   app.set_help_all_flag("--help-all", "Expand all help");
-  // Shell s("/tmp/cbsd.sock");
   auto construct = app.add_subcommand("construct", "Construct resource");
-  construct->add_option("-j,--jail", jail, "Jail name")->required();
-  app.add_subcommand("start", "Start resource");
-  app.add_subcommand("stop", "Stop resource");
-  app.require_subcommand(1);
+  construct->add_option("jail", jails, "Jail name")->required();
+  auto start = app.add_subcommand("start", "Start resource");
+  start->add_option("jail", jails, "Jail name")->required();
+  auto stop = app.add_subcommand("stop", "Stop resource");
+  stop->add_option("jail", jails, "Jail name")->required();
   CLI11_PARSE(app, argc, argv);
-  std::cout << app.get_subcommands()[0]->get_name() << ' ' << jail << std::endl;
+  Shell s("/tmp/cbsd.sock");
+  if (app.get_subcommands().size() == 0)
+  {
+    std::cout << "Interactive shell" << std::endl;
+  }
+  else
+  {
+    std::cout << app.get_subcommands()[0]->get_name();
+    for (auto jail : jails)
+    {
+      std::cout << ' ' << jail;
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
--- sock/shell/shell.cpp
+++ sock/shell/shell.cpp
@@ -1,21 +1,16 @@
+#include "shell.h"
+
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include "shell.h"
 
-
-Shell::Shell(const std::string &socket_path)
-  : socketPath{socket_path}
+Shell::Shell(const std::string &socket_path) : socketPath{socket_path}
 {
   struct sockaddr_un addr;
-  char buf[128];
-  int rc;
-  const char * const quit = "quit";
-  int quit_size = strlen(quit);
 
   if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
   {
@@ -25,21 +20,17 @@ Shell::Shell(const std::string &socket_path)
 
   memset(&addr, 0, sizeof(addr));
   addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socketPath.data(), sizeof(addr.sun_path)-1);
+  strncpy(addr.sun_path, socketPath.data(), sizeof(addr.sun_path) - 1);
 
-  if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+  if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
   {
+      std::cerr << "Connecting to " << socketPath << " failed: ";
     std::cerr << strerror(errno) << std::endl;
     exit(-1);
   }
 }
 
-
-Shell::~Shell()
-{
-  close(fd);
-}
-
+Shell::~Shell() { close(fd); }
 
 int Shell::put(const std::string &data)
 {
